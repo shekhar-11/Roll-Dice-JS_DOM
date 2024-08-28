24eb1480603430a24c9fdf46efb08261fdce7981
shekhar-11 <rajshekhar111201@gmail.com>
2024-08-29 00:55:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,60 @@
 'use strict';
+let instant_Score_0 = 0;
+let instant_Score_1 = 0;
 
 let changeValue = function () {
   if (current_player.classList.contains('player--0')) {
     current_player.classList.remove('player--active');
+
+    if (diceNum == '1') {
+      instant_Score_0 = 0;
+    } else {
+      hs_0(
+        Number(document.querySelector('#current--0').textContent) +
+          Number(document.querySelector('#score--0').textContent)
+      );
+      if (
+        current_player.classList.contains('player--0') &&
+        current_player.querySelector('#score--0').textContent >= 100
+      ) {
+        document.querySelector('main').style.backgroundColor = '#222';
+        document.querySelector('main').style.color = '#fff';
+        document.querySelector('body').classList.add('change_bg');
+
+        document.querySelector('#modal').classList.add('modal');
+        document.querySelector('.won').textContent =
+          'Player 0 Has Won The Game';
+      }
+    }
     current_player = document.querySelector('.player--1');
     current_player.classList.add('player--active');
-    if (diceNum == '1') {
-      curr_0('0');
-    } else if (
-      document.querySelector('#current--0').textContent >
-      document.querySelector('#score--0').textContent
-    ) {
-      hs_0(document.querySelector('#current--0').textContent);
-      curr_0('0');
-    }
+    curr_0('0');
   } else if (current_player.classList.contains('player--1')) {
     current_player.classList.remove('player--active');
+
+    if (diceNum == '1') {
+      instant_Score_1 = 0;
+    } else {
+      hs_1(
+        Number(document.querySelector('#current--1').textContent) +
+          Number(document.querySelector('#score--1').textContent)
+      );
+      if (
+        current_player.classList.contains('player--1') &&
+        current_player.querySelector('#score--1').textContent >= 100
+      ) {
+        document.querySelector('main').style.backgroundColor = '#222';
+        document.querySelector('main').style.color = '#fff';
+        document.querySelector('body').classList.add('change_bg');
+
+        document.querySelector('#modal').classList.add('modal');
+        document.querySelector('.won').textContent =
+          'Player 1 Has Won The Game';
+      }
+    }
     current_player = document.querySelector('.player--0');
     current_player.classList.add('player--active');
-    if (diceNum == '1') {
-      curr_1('0');
-    } else if (
-      document.querySelector('#current--1').textContent >
-      document.querySelector('#score--1').textContent
-    ) {
-      hs_1(document.querySelector('#current--1').textContent);
-      curr_1('0');
-    }
+    curr_1('0');
   }
 };
 const resetGame = document.querySelector('.btn--new');
@@ -51,6 +79,8 @@ resetGame.addEventListener('click', function () {
   hs_0('0');
   curr_0('0');
   curr_1('0');
+  instant_Score_0 = 0;
+  instant_Score_1 = 0;
 });
 
 let hold = document.querySelector('.btn--hold');
@@ -69,13 +99,64 @@ roll.addEventListener('click', function () {
     changeValue('1');
   } else if (diceNum === 2) {
     diceImg.src = 'dice-2.png';
+    if (current_player.classList.contains('player--0')) {
+      instant_Score_0 += 2;
+      curr_0(instant_Score_0);
+    } else {
+      instant_Score_1 += 2;
+      curr_1(instant_Score_1);
+    }
   } else if (diceNum === 3) {
     diceImg.src = 'dice-3.png';
+    if (current_player.classList.contains('player--0')) {
+      instant_Score_0 += 3;
+      curr_0(instant_Score_0);
+    } else {
+      instant_Score_1 += 3;
+      curr_1(instant_Score_1);
+    }
   } else if (diceNum === 4) {
     diceImg.src = 'dice-4.png';
+    if (current_player.classList.contains('player--0')) {
+      instant_Score_0 += 4;
+      curr_0(instant_Score_0);
+    } else {
+      instant_Score_1 += 4;
+      curr_1(instant_Score_1);
+    }
   } else if (diceNum === 5) {
     diceImg.src = 'dice-5.png';
+    if (current_player.classList.contains('player--0')) {
+      instant_Score_0 += 5;
+      curr_0(instant_Score_0);
+    } else {
+      instant_Score_1 += 5;
+      curr_1(instant_Score_1);
+    }
   } else if (diceNum === 6) {
     diceImg.src = 'dice-6.png';
+    if (current_player.classList.contains('player--0')) {
+      instant_Score_0 += 6;
+      curr_0(instant_Score_0);
+    } else {
+      instant_Score_1 += 6;
+      curr_1(instant_Score_1);
+    }
   }
+});
+
+document.querySelector('body').addEventListener('keydown', function (e) {
+  if (
+    document.querySelector('#modal').classList.contains('modal') &&
+    e.key === 'Escape'
+  ) {
+    document.querySelector('.won').textContent = '';
+    document.querySelector('#modal').classList.remove('modal');
+  }
+  hs_1('0');
+  hs_0('0');
+  curr_0('0');
+  curr_1('0');
+  instant_Score_0 = 0;
+  instant_Score_1 = 0;
 });

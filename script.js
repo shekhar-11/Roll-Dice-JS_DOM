dd4d7e235e81a2aad3ce2bdfa83349a0b0ea1d60
shekhar-11 <rajshekhar111201@gmail.com>
2024-08-29 15:02:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,25 @@
 let instant_Score_0 = 0;
 let instant_Score_1 = 0;
 
+document.querySelector('#input_submit').addEventListener('click', function () {
+  if (
+    Number(document.querySelector('#ip0').value.length) > 0 &&
+    Number(document.querySelector('#ip1').value.length) > 0
+  ) {
+    document.querySelector('#name--0').textContent =
+      document.querySelector('#ip0').value;
+    document.querySelector('#name--1').textContent =
+      document.querySelector('#ip1').value;
+    document.querySelector('#beginning').classList.add('beginning');
+  } else if (Number(document.querySelector('#ip0').value.length) === 0) {
+    document.querySelector('#ip0').placeholder = "FIELD CAN'T BE EMPTY!";
+  } else if (Number(document.querySelector('#ip1').value.length) === 0) {
+    document.querySelector('#ip1').placeholder = "FIELD CAN'T BE EMPTY!";
+  }
+});
+
+let current_player = document.querySelector('.player--0');
+
 let changeValue = function () {
   if (current_player.classList.contains('player--0')) {
     current_player.classList.remove('player--active');
@@ -20,8 +39,8 @@ let changeValue = function () {
         document.querySelector('#all').classList.add('change_bg');
 
         document.querySelector('#modal').classList.add('modal');
-        document.querySelector('.won').textContent =
-          'Player 0 Has Won The Game';
+        document.querySelector('.won').textContent = `
+        ${document.querySelector('#name--0').textContent} Has Won The Game`;
       }
     }
     instant_Score_0 = 0;
@@ -45,8 +64,8 @@ let changeValue = function () {
         document.querySelector('#all').classList.add('change_bg');
 
         document.querySelector('#modal').classList.add('modal');
-        document.querySelector('.won').textContent =
-          'Player 1 Has Won The Game';
+        document.querySelector('.won').textContent = `
+        ${document.querySelector('#name--1').textContent} Has Won The Game`;
       }
     }
     instant_Score_1 = 0;
@@ -55,9 +74,7 @@ let changeValue = function () {
     curr_1('0');
   }
 };
-const resetGame = document.querySelector('.btn--new');
-// const hold_p1 =
-// const hold_p2 =
+
 let hs_0 = function (value) {
   document.querySelector('#score--0').textContent = value;
 };
@@ -71,7 +88,7 @@ let curr_1 = function (value) {
   document.querySelector('#current--1').textContent = value;
 };
 
-let current_player = document.querySelector('.player--0');
+const resetGame = document.querySelector('.btn--new');
 resetGame.addEventListener('click', function () {
   hs_1('0');
   hs_0('0');
@@ -79,21 +96,17 @@ resetGame.addEventListener('click', function () {
   curr_1('0');
   instant_Score_0 = 0;
   instant_Score_1 = 0;
-  if (document.querySelector('#modal').classList.contains('modal')) {
-    document.querySelector('.won').textContent = '';
-    document.querySelector('#modal').classList.remove('modal');
-  }
-  document.querySelector('.main').style.backgroundColor =
+
+  document.querySelector('main').style.backgroundColor =
     'rgba(255, 255, 255, 0.35)';
   document.querySelector('#all').classList.remove('change_bg');
+  document.querySelector('.won').textContent = '';
+  document.querySelector('#modal').classList.remove('modal');
+  document.querySelector('#beginning').classList.remove('beginning');
 });
 
-let hold = document.querySelector('.btn--hold');
-
-hold.addEventListener('click', changeValue);
-
+//roll
 let diceNum = Math.trunc(Math.random() * 5) + 1;
-
 let diceImg = document.querySelector('.dice');
 let roll = document.querySelector('.btn--roll');
 roll.addEventListener('click', function () {
@@ -149,6 +162,7 @@ roll.addEventListener('click', function () {
     }
   }
 });
+// roll ends
 
 document.querySelector('body').addEventListener('keydown', function (e) {
   if (
@@ -175,3 +189,32 @@ document.querySelector('body').addEventListener('keydown', function (e) {
     document.querySelector('#all').classList.remove('change_bg');
   }
 });
+
+document.querySelector('#exit').addEventListener('click', function () {
+  document.querySelector('.won').textContent = '';
+  document.querySelector('#modal').classList.remove('modal');
+
+  hs_1('0');
+  hs_0('0');
+  curr_0('0');
+  curr_1('0');
+  instant_Score_0 = 0;
+  instant_Score_1 = 0;
+
+  // document.querySelector('.all').classList.remove('change_bg');
+  if (
+    document.querySelector('main').style.backgroundColor !=
+    'rgba(255, 255, 255, 0.35)'
+  ) {
+    document.querySelector('main').style.backgroundColor =
+      'rgba(255, 255, 255, 0.35)';
+    document.querySelector('#all').classList.remove('change_bg');
+  }
+});
+
+// hold
+let hold = document.querySelector('.btn--hold');
+
+hold.addEventListener('click', changeValue);
+
+// hold ends
